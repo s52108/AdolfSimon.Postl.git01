8de7213c5cd1d68575762a5454d380fac952f0d2
postl <s52108@edu.campus02.at>
2022-09-20 12:47:02 +0200

    Added "Hier könnte meine Matrikelnummer stehen 12345678

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,5 +3,7 @@ public class HelloGit {
     public static void main(String[] args) {
         //sout+TAB
         System.out.println("Hello Git");
+        //strg+D
+        System.out.println("Hier könnte meine Matrikelnummer stehen 12345678");
     }
 }
